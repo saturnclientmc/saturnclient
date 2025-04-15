9fb4ad32bf764e93bb4b1da295151604d90f2022
Leo dev <klesti.selimay@icloud.com>
2025-04-15 13:01:31 +0200

    bug fix: iris support

--- src/main/java/org/saturnclient/saturnclient/mixin/NameTagMixin.java
+++ src/main/java/org/saturnclient/saturnclient/mixin/NameTagMixin.java
@@ -20,7 +20,7 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-@Mixin(EntityRenderer.class)
+@Mixin(value = EntityRenderer.class, priority = 1500)
 public abstract class NameTagMixin<S extends EntityRenderState> {
     @Shadow
     public EntityRenderDispatcher dispatcher;
