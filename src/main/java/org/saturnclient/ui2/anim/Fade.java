5b6810a32f65fa4ae30e551e0f3d8186156dba6c
Leo dev <klesti.selimay@icloud.com>
2025-04-30 07:52:30 -0500

    removed borders

--- src/main/java/org/saturnclient/ui2/anim/Fade.java
+++ src/main/java/org/saturnclient/ui2/anim/Fade.java
@@ -3,18 +3,20 @@ package org.saturnclient.ui2.anim;
 import org.saturnclient.ui2.Element;
 
 public class Fade extends Animation {
+    float targetOpacity;
+
     public Fade(int duration) {
         super(duration);
     }
 
     @Override
     public void tick(float progress, Element element) {
-        element.opacity = progress;
-        // element.opacity += 0.1f;
+        element.opacity = targetOpacity * progress;
     }
 
     @Override
     public void init(Element element) {
+        targetOpacity = element.opacity;
         element.opacity = 0;
     }
 }
--- src/main/java/org/saturnclient/ui2/elements/Button.java
+++ src/main/java/org/saturnclient/ui2/elements/Button.java
@@ -13,14 +13,11 @@ public class Button extends Element {
     private static ThemeManager theme = new ThemeManager("Button", "hovering");
     private static Property<Integer> bgColor = theme.property("bg-color", new Property<Integer>(0xFF000000));
     private static Property<Integer> fgColor = theme.property("fg-color", new Property<Integer>(0xFFFFFFFF));
-    private static Property<Integer> borderColor = theme.property("border-color", new Property<Integer>(0xFFFFFFFF));
-    private static Property<Integer> border = theme.property("border", new Property<Integer>(1));
     private static Property<Integer> cornerRadius = theme.property("corner-radius", new Property<Integer>(10));
     private static Property<Boolean> bold = theme.property("fg-bold", new Property<Boolean>(false));
 
     static {
         theme.propertyStateDefault("hovering", "fg-color", 0xFFFF0000);
-        theme.propertyStateDefault("hovering", "border-color", 0xFFFF0000);
     }
 
     private String text;
@@ -42,8 +39,6 @@ public class Button extends Element {
             theme.setState(null);
         }
         renderScope.drawRoundedRectangle(0, 0, width, height, cornerRadius.value, bgColor.value);
-
-        renderScope.drawRoundedBorder(width, height, border.value, cornerRadius.value, borderColor.value);
 
         renderScope.drawText(text, (width - Fonts.getWidth(text, font)) / 2, (height - Fonts.getHeight()) / 2, bold.value, fgColor.value);
     }
