a1c68609d9b30223bdf126220b9dc328f28af226
Leo dev <klesti.selimay@icloud.com>
2025-06-14 04:21:34 +0200

    working emote test

--- src/main/java/org/saturnclient/saturnclient/event/KeyInputHandler.java
+++ src/main/java/org/saturnclient/saturnclient/event/KeyInputHandler.java
@@ -3,12 +3,16 @@ package org.saturnclient.saturnclient.event;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
 import net.minecraft.client.option.KeyBinding;
-// import net.minecraft.client.option.Perspective;
 import net.minecraft.client.util.InputUtil;
+import net.minecraft.util.Identifier;
 
-// import org.saturnclient.modules.mods.FreeLook;
+import org.saturnclient.saturnclient.SaturnClient;
 import org.saturnclient.ui2.screens.ShiftMenu;
-// import org.saturnclient.saturnclient.menus.SaturnMenu;
+
+import dev.kosmx.playerAnim.api.layered.AnimationStack;
+import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationAccess;
+import dev.kosmx.playerAnim.minecraftApi.PlayerAnimationRegistry;
+
 import org.lwjgl.glfw.GLFW;
 
 /**
@@ -21,55 +25,38 @@ public class KeyInputHandler {
     private static final int DEFAULT_MENU_KEY = GLFW.GLFW_KEY_RIGHT_SHIFT;
     private static KeyBinding mainMenuKeyBinding;
 
-    // private static final String FREELOOK_KEY_ID = "key.saturnclient.freelook";
-    // private static final int FREELOOK_KEY = GLFW.GLFW_KEY_M;
-    // private static KeyBinding freelookKeyBinding;
+    private static final String FREELOOK_KEY_ID = "key.saturnclient.emote";
+    private static final int FREELOOK_KEY = GLFW.GLFW_KEY_B;
+    private static KeyBinding freelookKeyBinding;
 
-    /**
-     * Registers all keybindings and their associated event handlers.
-     * Should be called during mod initialization.
-     */
     public static void register() {
-        // Register the main menu key binding
         mainMenuKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
                 MAIN_MENU_KEY_ID,
                 InputUtil.Type.KEYSYM,
                 DEFAULT_MENU_KEY,
                 KEY_CATEGORY));
 
-        // freelookKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
-        //         FREELOOK_KEY_ID,
-        //         InputUtil.Type.KEYSYM,
-        //         FREELOOK_KEY,
-        //         KEY_CATEGORY));
+        freelookKeyBinding = KeyBindingHelper.registerKeyBinding(new KeyBinding(
+                FREELOOK_KEY_ID,
+                InputUtil.Type.KEYSYM,
+                FREELOOK_KEY,
+                KEY_CATEGORY));
 
         // Register the event handler for the main menu key
         ClientTickEvents.END_CLIENT_TICK.register(client -> {
             if (mainMenuKeyBinding.wasPressed()) {
                 client.setScreen(new ShiftMenu());
             }
-            // else if (freelookKeyBinding.wasPressed() && FreeLook.enabled.value && FreeLook.toggle.value) {
-            //     FreeLook.isFreeLooking = !FreeLook.isFreeLooking;
-            //     if (FreeLook.isFreeLooking) {
-            //         FreeLook.lastPerspective = client.options.getPerspective();
-            //         client.options.setPerspective(Perspective.THIRD_PERSON_BACK);
-            //     } else if (FreeLook.lastPerspective != null) {
-            //         client.options.setPerspective(FreeLook.lastPerspective);
-            //         FreeLook.lastPerspective = null;
-            //     }
-            // } else if (FreeLook.enabled.value && !FreeLook.toggle.value) {
-            //     if (freelookKeyBinding.isPressed()) {
-            //         if (!FreeLook.isFreeLooking) {
-            //             FreeLook.lastPerspective = client.options.getPerspective();
-            //             client.options.setPerspective(Perspective.THIRD_PERSON_BACK);
-            //             FreeLook.isFreeLooking = true;
-            //         }
-            //     } else if (FreeLook.isFreeLooking && FreeLook.lastPerspective != null) {
-            //         client.options.setPerspective(FreeLook.lastPerspective);
-            //         FreeLook.isFreeLooking = false;
-            //         FreeLook.lastPerspective = null;
-            //     }
-            // }
+
+            if (SaturnClient.client.player != null && SaturnClient.client.player.isSneaking()) {
+                AnimationStack animationStack = PlayerAnimationAccess.getPlayerAnimLayer(SaturnClient.client.player);
+                animationStack.removeLayer(1000);
+            }
+
+            if (freelookKeyBinding.wasPressed()) {
+                AnimationStack animationStack = PlayerAnimationAccess.getPlayerAnimLayer(SaturnClient.client.player);
+                animationStack.addAnimLayer(1000, PlayerAnimationRegistry.getAnimation(Identifier.of("saturnclient", "backflip")).playAnimation());
+            }
         });
     }
 }
