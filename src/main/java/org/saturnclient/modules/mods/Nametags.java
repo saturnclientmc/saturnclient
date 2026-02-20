2bcad834097b8a922da159d95c4d9bb4ba4983ed
RudideC <rudidecastrop@gmail.com>
2026-02-20 09:34:55 +0100

    added nametags logo

--- src/main/java/org/saturnclient/modules/mods/Nametags.java
+++ src/main/java/org/saturnclient/modules/mods/Nametags.java
@@ -7,13 +7,15 @@ import org.saturnclient.saturnclient.config.Property;
 
 import net.minecraft.client.render.entity.state.LivingEntityRenderState;
 
+// todo: rendering modes, player check
+
 public class Nametags extends Module {
     private static Property<Boolean> enabled = Property.bool(false);
     private static Property<Boolean> healthDisplay = Property.bool(false);
     
     public Nametags() {
         super(
-            new ModuleDetails("Nametags", "zoom")
+            new ModuleDetails("Nametags", "nametags")
                 .description("Allows you to modify entity nametags")
                 .tags("Camera", "Utility")
                 .version("0.1.0"),
@@ -36,8 +38,8 @@ public class Nametags extends Module {
         float health = hrs.saturn$getHealth();
         float maxHealth = hrs.saturn$getMaxHealth();
 
-        if (health <= 0f || maxHealth <= 0f || health > maxHealth) return null;
-        
+        if (health < 0f || maxHealth < 0f || health > maxHealth) return null;
+
         return state.customName.getString()
             + " §c" + String.format("%.1f", hrs.saturn$getHealth())
             + " / " + String.format("%.1f", hrs.saturn$getMaxHealth()) + " ❤";
