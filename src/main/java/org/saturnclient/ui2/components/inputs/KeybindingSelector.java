3bc21a5b30f08ff6818fda8c1b497be9ab6078b1
Klesti Selimaj <klesti.selimay@icloud.com>
2026-02-21 18:19:44 +0100

    Keybinding selector and select

--- src/main/java/org/saturnclient/ui2/components/inputs/KeybindingSelector.java
+++ src/main/java/org/saturnclient/ui2/components/inputs/KeybindingSelector.java
@@ -2,15 +2,13 @@ package org.saturnclient.ui2.components.inputs;
 
 import org.lwjgl.glfw.GLFW;
 import org.saturnclient.saturnclient.config.Property;
-import org.saturnclient.saturnclient.config.ThemeManager;
+import org.saturnclient.saturnclient.config.Theme;
 import org.saturnclient.ui2.Element;
 import org.saturnclient.ui2.ElementContext;
 import org.saturnclient.ui2.RenderScope;
 import org.saturnclient.ui2.resources.Fonts;
 
 public class KeybindingSelector extends Element {
-    private static ThemeManager theme = new ThemeManager("Input");
-    private static Property<Integer> font = theme.property("font", Property.font(1));
     Property<Integer> prop;
 
     public KeybindingSelector(Property<Integer> prop) {
@@ -25,7 +23,7 @@ public class KeybindingSelector extends Element {
         renderScope.drawRoundedRectangle(0, 0, width, height, 10, 0xFF000000);
         renderScope.drawText(0.6f, prop.value == -1 ? "<NONE>" :
             GLFW.glfwGetKeyName(prop.value, GLFW.glfwGetKeyScancode(prop.value)).toUpperCase(),
-            4, 4, font.value, textColor);
+            4, 4, Theme.FONT.value, textColor);
     }
 
     @Override
