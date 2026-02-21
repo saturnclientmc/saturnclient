79ceaf67a12c121ab5c53a8576778eb46d26617c
Klesti Selimaj <klesti.selimay@icloud.com>
2026-02-21 18:28:30 +0100

    Cosmetic Preview

--- src/main/java/org/saturnclient/ui2/components/CosmeticPreview.java
+++ src/main/java/org/saturnclient/ui2/components/CosmeticPreview.java
@@ -1,7 +1,6 @@
 package org.saturnclient.ui2.components;
 
-import org.saturnclient.saturnclient.config.Property;
-import org.saturnclient.saturnclient.config.ThemeManager;
+import org.saturnclient.saturnclient.config.Theme;
 import org.saturnclient.ui2.Element;
 import org.saturnclient.ui2.ElementContext;
 import org.saturnclient.ui2.RenderScope;
@@ -10,13 +9,8 @@ import net.minecraft.client.render.RenderLayer;
 import net.minecraft.util.Identifier;
 
 public class CosmeticPreview extends Element {
-    private static ThemeManager theme = new ThemeManager("CosmeticPreview", "selected", "hovering");
-    private static Property<Integer> bgColor = theme.property("bg-color", Property.color(0x89000000));
-    private static Property<Integer> fgColor = theme.property("fg-color", Property.color(0xFFFFFFFF));
-    private static Property<Integer> padding = theme.property("padding", Property.integer(14));
-    private static Property<Integer> cornerRadius = theme.property("corner-radius", Property.integer(10));
-
-    static {}
+    static {
+    }
 
     Identifier sprite;
     private Runnable onClick;
@@ -30,25 +24,19 @@ public class CosmeticPreview extends Element {
         this.width = 50;
         this.height = 111;
     }
-    
+
     @Override
     public void render(RenderScope renderScope, ElementContext ctx) {
-        if (ctx.isHovering()) {
-            theme.setState("hovering");
-        } else if (isSelected) {
-            theme.setState("selected");
-        }
+        renderScope.drawRoundedRectangle(0, 0, width, height, Theme.WIDGET_RADIUS.value,
+                isSelected ? Theme.ACCENT.value : Theme.PRIMARY.value);
 
-        renderScope.drawRoundedRectangle(0, 0, width, height, cornerRadius.value, bgColor.value);
-        
         renderScope.setRenderLayer(RenderLayer::getGuiTextured);
 
-        int texWidth = width - padding.value;
-        int texHeight = height - padding.value;
-        
-        renderScope.drawTexture(sprite, padding.value / 2, padding.value / 2, 0, 0, texWidth, texHeight, fgColor.value);
+        int texWidth = width - Theme.PADDING.value;
+        int texHeight = height - Theme.PADDING.value;
 
-        theme.setState(null);
+        renderScope.drawTexture(sprite, Theme.PADDING.value / 2, Theme.PADDING.value / 2, 0, 0, texWidth, texHeight,
+                Theme.FOREGROUND.value);
     }
 
     @Override
