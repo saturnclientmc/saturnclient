39cea1418348b349a1d11db8f935a14038ffb055
Klesti Selimaj <klesti.selimay@icloud.com>
2026-02-21 18:10:57 +0100

    Theme padding and radius

--- src/main/java/org/saturnclient/saturnclient/config/Theme.java
+++ src/main/java/org/saturnclient/saturnclient/config/Theme.java
@@ -7,6 +7,9 @@ public class Theme {
     public static Property<Integer> PRIMARY_FG = Property.color(0x00000000);
     public static Property<Integer> ACCENT = Property.color(0x00000000);
     public static Property<Integer> ACCENT_FG = Property.color(0x00000000);
+    public static Property<Integer> BG_RADIUS = Property.integer(10);
+    public static Property<Integer> WIDGET_RADIUS = Property.integer(10);
+    public static Property<Integer> PADDING = Property.integer(15);
 
     public static void init(ConfigManager parent) {
         ConfigManager config = new ConfigManager(parent, "UI Style");
@@ -16,5 +19,7 @@ public class Theme {
         config.property("Primary Foreground", PRIMARY_FG);
         config.property("Accent", ACCENT);
         config.property("Accent Foreground", ACCENT_FG);
+        config.property("Background Radius", BG_RADIUS);
+        config.property("Widget Radius", WIDGET_RADIUS);
     }
 }
