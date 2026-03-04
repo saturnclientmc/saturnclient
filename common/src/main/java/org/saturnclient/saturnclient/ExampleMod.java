3d7d96552af938c14c9ecfbbffcde3170fd30816
Klesti Selimaj <klesti.selimay@icloud.com>
2026-03-04 16:26:40 +0100

    Using template from https://template.lewds.dev/

---
+++ common/src/main/java/org/saturnclient/saturnclient/ExampleMod.java
@@ -0,0 +1,9 @@
+package org.saturnclient.saturnclient;
+
+public class ExampleMod {
+    public static final String MOD_ID = "saturnclient";
+
+    public static void init() {
+        System.out.println("Hello World!");
+    }
+}
---
+++ fabric/src/main/java/org/saturnclient/saturnclient/ExampleModFabric.java
@@ -0,0 +1,10 @@
+package org.saturnclient.saturnclient;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExampleModFabric implements ModInitializer {
+    @Override
+    public void onInitialize() {
+        ExampleMod.init();
+    }
+}
