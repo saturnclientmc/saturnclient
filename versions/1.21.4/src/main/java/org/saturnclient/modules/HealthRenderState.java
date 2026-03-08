d68e47d1cc1eba384fcb1a8e177cfa705cfb152a
Klesti Selimaj <klesti.selimay@icloud.com>
2026-03-08 13:48:06 +0100

    Removed old module logic

--- versions/1.21.4/src/main/java/org/saturnclient/modules/HealthRenderState.java
+++ versions/1.21.4/src/main/java/org/saturnclient/modules/HealthRenderState.java
@@ -1,15 +1,22 @@
 package org.saturnclient.modules;
 
-import org.saturnclient.common.module.EntityModule.EntityType;
+import org.saturnclient.common.module.EntityModule;
 
+/**
+ * Mixed into {@link net.minecraft.client.render.entity.state.LivingEntityRenderState}
+ * by {@link org.saturnclient.modules.mixins.LivingEntityRenderStateMixin}.
+ *
+ * Carries the health snapshot and entity classification that
+ * {@link org.saturnclient.modules.mixins.NametagsMixin} needs without
+ * accessing the live entity during rendering.
+ */
 public interface HealthRenderState {
-    float saturn$getHealth();
 
+    float saturn$getHealth();
     float saturn$getMaxHealth();
 
     void saturn$setHealth(float health, float maxHealth);
 
-    EntityType saturn$getEntityType();
-
-    void saturn$setEntityType(EntityType type);
+    EntityModule.EntityType saturn$getEntityType();
+    void saturn$setEntityType(EntityModule.EntityType type);
 }
