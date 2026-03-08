b04014a87f99c38a07687ced67e2a53b40dd5d64
Klesti Selimaj <klesti.selimay@icloud.com>
2026-03-08 13:34:52 +0100

    Refactor mixins

---
+++ versions/1.21.4/src/main/java/org/saturnclient/modules/mixins/HealthRenderState.java
@@ -0,0 +1,22 @@
+package org.saturnclient.modules.mixins;
+
+import org.saturnclient.common.module.EntityModule;
+
+/**
+ * Mixed into {@link net.minecraft.client.render.entity.state.LivingEntityRenderState}
+ * by {@link org.saturnclient.modules.mixins.LivingEntityRenderStateMixin}.
+ *
+ * Carries the health snapshot and entity classification that
+ * {@link org.saturnclient.modules.mixins.NametagsMixin} needs without
+ * accessing the live entity during rendering.
+ */
+public interface HealthRenderState {
+
+    float saturn$getHealth();
+    float saturn$getMaxHealth();
+
+    void saturn$setHealth(float health, float maxHealth);
+
+    EntityModule.EntityType saturn$getEntityType();
+    void saturn$setEntityType(EntityModule.EntityType type);
+}
