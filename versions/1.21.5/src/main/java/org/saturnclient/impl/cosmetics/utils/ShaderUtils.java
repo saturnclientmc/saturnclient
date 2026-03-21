52930df0a97e55dacbdf41db81f02a4945cd188f
RudideC <rudidecastrop@gmail.com>
2026-03-21 16:04:22 +0100

    copy pasted 1.21.4 haha

---
+++ versions/1.21.5/src/main/java/org/saturnclient/impl/cosmetics/utils/ShaderUtils.java
@@ -0,0 +1,35 @@
+package org.saturnclient.impl.cosmetics.utils;
+
+import net.minecraft.client.render.RenderLayer;
+import net.minecraft.client.render.RenderPhase;
+import net.minecraft.client.render.VertexFormat.DrawMode;
+import net.minecraft.client.render.VertexFormats;
+import net.minecraft.client.render.RenderPhase.Textures;
+import net.minecraft.client.render.block.entity.EndPortalBlockEntityRenderer;
+import net.minecraft.util.Identifier;
+
+public class ShaderUtils {
+    public static final RenderLayer END_GATEWAY = RenderLayer.of(
+            "end_gateway_no_cull",
+            VertexFormats.POSITION,
+            DrawMode.QUADS,
+            1536,
+            false,
+            false,
+            RenderLayer.MultiPhaseParameters.builder()
+                    .program(RenderLayer.END_GATEWAY_PROGRAM)
+                    .texture(Textures.create()
+                            .add(EndPortalBlockEntityRenderer.SKY_TEXTURE, false, false)
+                            .add(EndPortalBlockEntityRenderer.PORTAL_TEXTURE, false, false)
+                            .build())
+                    .cull(RenderPhase.DISABLE_CULLING)
+                    .build(false));
+
+    public static RenderLayer getRenderLayer(Identifier texture) {
+        if (texture.getPath().endsWith("end.png")) {
+            return END_GATEWAY;
+        } else {
+            return RenderLayer.getEntityAlpha(texture);
+        }
+    }
+}
---
+++ versions/1.21.5/src/main/java/org/saturnclient/impl/modules/entity/HealthRenderState.java
@@ -0,0 +1,20 @@
+package org.saturnclient.impl.modules.entity;
+
+import org.saturnclient.common.module.EntityModule;
+
+/**
+ * Injected interface on {@link net.minecraft.client.render.entity.state.LivingEntityRenderState}.
+ *
+ * Carries the entity's health snapshot and classification into the render
+ * pipeline so that {@link NametagsMixin} can read them without touching
+ * the live entity again.
+ */
+public interface HealthRenderState {
+
+    float saturn$getHealth();
+    float saturn$getMaxHealth();
+    void  saturn$setHealth(float health, float maxHealth);
+
+    EntityModule.EntityType saturn$getEntityType();
+    void saturn$setEntityType(EntityModule.EntityType type);
+}
