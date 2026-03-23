faa18c99ab8b0c86d6bc490d9acc3f84ee57c528
Klesti Selimaj <klesti.selimay@icloud.com>
2026-03-23 14:33:26 +0100

    Fixed hat rotation in 1.21.5

--- versions/1.21.5/src/main/java/org/saturnclient/impl/cosmetics/HatFeatureRenderer.java
+++ versions/1.21.5/src/main/java/org/saturnclient/impl/cosmetics/HatFeatureRenderer.java
@@ -1,5 +1,6 @@
 package org.saturnclient.impl.cosmetics;
 
+import org.joml.Quaternionf;
 import org.saturnclient.client.player.SaturnPlayer;
 import org.saturnclient.impl.cosmetics.obj.MtlLoader;
 import org.saturnclient.impl.cosmetics.obj.ObjModel;
@@ -12,8 +13,6 @@ import net.minecraft.client.render.entity.model.PlayerEntityModel;
 import net.minecraft.client.render.entity.state.PlayerEntityRenderState;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.client.model.ModelPart;
-
-import org.joml.Quaternionf;
 
 public class HatFeatureRenderer extends FeatureRenderer<PlayerEntityRenderState, PlayerEntityModel> {
     public HatFeatureRenderer(FeatureRendererContext<PlayerEntityRenderState, PlayerEntityModel> context) {
@@ -30,10 +29,9 @@ public class HatFeatureRenderer extends FeatureRenderer<PlayerEntityRenderState,
         }
 
         matrices.push();
-        
+
         ModelPart head = this.getContextModel().head;
-        matrices.translate(head.originX / 16f, head.originY / 16f, head.originZ / 16f);
-        matrices.multiply(new Quaternionf().rotationXYZ(head.pitch, head.yaw, head.roll));
+        matrices.multiply(new Quaternionf().rotationZYX(head.roll, head.yaw, head.pitch));
 
         ObjModel.cosmetic("hat", player.hat).render(MtlLoader.cosmetic(player.hat),
                 matrices, vertexConsumers, light,
