92a1f70eb10a5b3707089af71bb3c54b9ea68d58
Klesti Selimaj <klesti.selimay@icloud.com>
2026-03-25 15:43:18 +0100

    Fixed DrawContextAccessor

--- versions/1.21.6/src/main/java/org/saturnclient/saturnclient/mixin/DrawContextAccessor.java
+++ versions/1.21.6/src/main/java/org/saturnclient/saturnclient/mixin/DrawContextAccessor.java
@@ -3,14 +3,10 @@ package org.saturnclient.saturnclient.mixin;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.render.state.GuiRenderState;
 import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.gen.Accessor;
 
 @Mixin(DrawContext.class)
-public abstract class DrawContextAccessor {
-    @Shadow
-    public GuiRenderState state;
-
-    public GuiRenderState getState() {
-        return state;
-    }
+public interface DrawContextAccessor {
+    @Accessor("state")
+    GuiRenderState getState();
 }
